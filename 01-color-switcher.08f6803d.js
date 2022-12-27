a1be60bf6085c4985e79da94541752a890d72559
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-27 18:04:40 +0000

    Deploying to gh-pages from @ OksanaGerzanych/goit-js-hw-09@848fdd40d605928c21644ec698ad84e182ccca32 🚀

---
+++ 01-color-switcher.08f6803d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(r){t.disabled=!0,d.disabled=!1,timerId=setInterval((()=>{let t=`#${Math.floor(16777215*Math.random()).toString(16)}`;e.style.backgroundColor=t}),1e3)})),d.addEventListener("click",(function(){t.disabled=!1,d.disabled=!0,clearInterval(timerId)}));
+//# sourceMappingURL=01-color-switcher.08f6803d.js.map
