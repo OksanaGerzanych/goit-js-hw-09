60d796607aaee05ff61f2365fee5e490791f547e
OksanaGerzanych <109728377+OksanaGerzanych@users.noreply.github.com>
2022-12-27 20:42:46 +0200

    add file

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,29 +1,27 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
 const bodyColor = document.querySelector("body");
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
 let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 btnStart.addEventListener('click', onClick);
 btnStop.addEventListener('click', onClickStop);
 
 function onClick() {
-    btnStart.disabled = true;
-    btnStop.disabled = false;
-    timerId = setInterval(() => {
-    let color = getRandomHexColor();
-    bodyColor.style.backgroundColor = color;
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+    
+  timerId = setInterval(() => {
+  let color = getRandomHexColor();
+  bodyColor.style.backgroundColor = color;
  },1000)
- 
 } 
 
 function onClickStop() {
-  
-  btnStart.disabled = false;
-  btnStop.disabled = true;
   clearInterval(timerId);
-  
+  btnStart.disabled = false;
+  btnStop.disabled = true;  
 }
